17ef90c5dc3447285a5a2637f0d6df3669d26a33
Rayckey <rocky_wang@live.com>
2021-06-13 22:11:24 -0700

    slip final

--- src/singlebot_control/src/singlebot_control.py
+++ src/singlebot_control/src/singlebot_control.py
@@ -3,8 +3,12 @@ import roslib; roslib.load_manifest('singlebot_control')
 import rospy, yaml, sys
 from gazebo_msgs.msg import ModelStates, ContactsState
 from sensor_msgs.msg import JointState , Imu
+from geometry_msgs.msg import Vector3
 from std_msgs.msg import Float64
 from rospy import numpy_msg
+import tf.transformations
+
+import tf2_ros
 import math
 import numpy as np
 
@@ -13,7 +17,7 @@ class SpringLoadedInvertedPendulum():
         nn = Imu()
         self.phase = 'flight'
         self.pos = [0,0,0]
-        self.vel = [0,0,0]
+        self.vel = Vector3()
         self.vel_desired = [0,0,0]
         self.angular_velocity_desired = [0,0,0]
         self.orientation = nn.orientation
@@ -27,16 +31,19 @@ class SpringLoadedInvertedPendulum():
         self.joint_position_desired = [0,0,0]
         self.joint_effort_desired = [0,0,0]
 
+        self.body_vel = [0,0,0]
+
+
         # setup joint parameters
-        self.k1 = 100
-        self.d1 = 10
-        self.k2 = 100
-        self.d2 = 10
+        self.k1 = 10
+        self.d1 = 1
+        self.k2 = 10
+        self.d2 = 1
         self.d3 = 10
 
         # set up SLIP parameters
         self.f = 1.0 # frequency
-        self.m = 3 + 0.3 + 0.3
+        self.m = 3 + 0.3 + 0.3 + 0.3
         self.k3 = (2*math.pi*self.f)**2 * self.m # needed spring constant
         print("k is " , self.k3 )
         self.Ts = math.pi*math.sqrt(self.m/self.k3)
@@ -48,14 +55,49 @@ class SpringLoadedInvertedPendulum():
         self.r0 = self.z_bottom + self.deltab
         self.leg_rest_length = 1.2
 
+    def calBodyVelocity(self):
+        # self.vel.x = 0
+        # self.vel.y = 0.5
+        # self.vel.z = 0
+        qu = tf.transformations.quaternion_matrix([self.orientation.x, self.orientation.y, self.orientation.z, self.orientation.w]) 
+        # stuff = np.dot(tf.transformations.inverse_matrix(qu) , tf.transformations.translation_matrix([self.vel.x,self.vel.y,self.vel.z])) 
+        stuff = np.dot(qu, tf.transformations.translation_matrix([self.vel.x,self.vel.y,self.vel.z])) 
+        self.body_vel = stuff[:3, 3]
+
+    def calTransformedAngle(self,jda):
+
+        print("jda is ", jda )
+        qu1 = tf.transformations.rotation_matrix(jda[0], np.array([0,0,1]))
+        qu2 = tf.transformations.rotation_matrix(jda[1], np.array([0,-1,0]))
+        qu3 = np.dot(qu1,qu2)
+        # print("rotation matrix is ", qu3 )
+        # pt1 = np.dot(tf.transformations.inverse_matrix(qu3),tf.transformations.translation_matrix( np.array([0,0,1]))) 
+        pt1 = np.dot(qu3,tf.transformations.translation_matrix( np.array([0,0,-1]))) 
+
+        print("leg in world is ", pt1[:3,3] )
+
+        qu = tf.transformations.quaternion_matrix([self.orientation.x, self.orientation.y, self.orientation.z, self.orientation.w]) 
+        pt2 = np.dot(tf.transformations.inverse_matrix(qu) , tf.transformations.translation_matrix(pt1[:3,3]))
+        # pt2 = np.dot(qu , tf.transformations.translation_matrix(pt1[:3,3]))
+        pt3 = pt2[:3,3]
+        print("leg in body is ", pt3 )
+
+        lam = np.unwrap ([math.atan2(pt3[1],pt3[0])], np.pi*2)
+        gamma = math.acos(-pt3[2])
+
+        print("new jda is ", (lam[0], gamma) )
+        return (lam[0], gamma)
+        
+
     def calTouchdownAngle(self):
-        gamma  = math.asin(   math.sqrt( self.vel[0]**2 + self.vel[1]**2)   * self.Ts / 2 / self.r0)
-        lam = math.atan2(self.vel[1], self.vel[0])
-        return (lam, gamma)
+        gamma  = np.unwrap ( [math.asin(   math.sqrt( self.vel.x**2 + self.vel.y**2)   * self.Ts / 2 / self.r0)])
+        lam = math.atan2(self.vel.y, self.vel.x)
+        return (lam, gamma[0])
         # gamma_y  = math.asin(self.vel(1) * self.Ts / 2 / self.r0)
 
     def updateState(self, states):
         self.orientation = states.orientation
+        # self.orientation = tf.transformations.quaternion_matrix([states.orientation.x,states.orientation.y,states.orientation.z,states.orientation.w])
         self.angular_velocity = states.angular_velocity
         self.linear_accelration = states.linear_acceleration
 
@@ -72,9 +114,9 @@ class SpringLoadedInvertedPendulum():
                 self.pos[0] = data.pose[i].position.x
                 self.pos[1] = data.pose[i].position.y
                 self.pos[2] = data.pose[i].position.z
-                self.vel[0] = data.twist[i].linear.x
-                self.vel[1] = data.twist[i].linear.y
-                self.vel[2] = data.twist[i].linear.z
+                self.vel.x = data.twist[i].linear.x
+                self.vel.y = data.twist[i].linear.y
+                self.vel.z = data.twist[i].linear.z
 
     def subContact(self, data):
         if len(data.states) == 0:
@@ -87,13 +129,21 @@ class SpringLoadedInvertedPendulum():
 
     def calJointAngle(self):
         # since desired angles are only applicable for flight 
+
         if self.phase == 'flight':
+            self.calBodyVelocity()
+            print(' world vel is ', self.vel)
+            # print(' body vel is ', self.body_vel)
             jda = self.calTouchdownAngle()
+            jda = self.calTransformedAngle(jda)
             jfl = self.r0 - self.leg_rest_length
             self.joint_position_desired[0] = jda[0]
             self.joint_position_desired[1] = jda[1]
             self.joint_position_desired[2] = jfl
             # print(self.joint_position_desired)
+        else: 
+            jfl = self.r0 - self.leg_rest_length
+            self.joint_position_desired[2] = jfl
         return
 
     def calJointEffort(self):
@@ -101,6 +151,8 @@ class SpringLoadedInvertedPendulum():
             self.joint_effort_desired[0] = self.k1 * (self.joint_position_desired[0] - self.joint_position[0]) - self.d1 *self.joint_velocity[0]
             self.joint_effort_desired[1] = self.k2 * (self.joint_position_desired[1] - self.joint_position[1]) - self.d2 *self.joint_velocity[1]
             self.joint_effort_desired[2] = self.k3 * (self.joint_position_desired[2] - self.joint_position[2]) - self.d3 *self.joint_velocity[2]
+            # print(' effort is ', self.joint_effort_desired)
+            # print(' foot error is ', self.joint_position_desired[2] - self.joint_position[2])
         else:
             self.joint_effort_desired[0] = 0
             self.joint_effort_desired[1] = 0
@@ -114,7 +166,7 @@ class SpringLoadedInvertedPendulum():
         self.joint_velocity[2] = data.velocity[0]
         self.joint_velocity[0] = data.velocity[1]
         self.joint_velocity[1] = data.velocity[2]
-        # print(self.joint_position)
+        # print("self.joint_position is " , self.joint_position)
         # print(data.position)
 
 
